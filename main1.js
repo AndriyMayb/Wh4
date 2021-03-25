59b7ccf85de87aacba2dad446ed135b95a39f65c
andriymay <omayborode@gmail.com>
2021-03-26 00:13:21 +0200

    wh4

---
+++ main1.js
@@ -0,0 +1,615 @@
+//Завдання №1
+
+
+/*
+let home = 'hello';
+let domen = 'owu';
+let domen1 = 'com';
+let domen2 = 'ua';
+let number = 1;
+let number1 = 10;
+let number2 = -999;
+let number3 = 123;
+let number4 = 3;
+let number5 = 14;
+let number6 = 2.7;
+let number7 = 16;
+let result1 = true;
+let result2 = false;
+
+console.log(home);
+console.log(domen);
+console.log(domen1);
+console.log(domen2);
+console.log(number);
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+console.log(number7);
+console.log(result1);
+console.log(result2);
+*/
+
+/*
+alert (home);
+alert(domen);
+alert(domen1);
+alert(domen2);
+alert(number);
+alert(number1);
+alert(number2);
+alert(number3);
+alert(number4);
+alert(number5);
+alert(number6);
+alert(number7);
+alert(result1);
+alert(result2);
+*/
+
+/*document.write(home);
+document.write(domen);
+document.write(domen1);
+document.write(domen2);
+document.write(number);
+document.write(number1);
+document.write(number2);
+document.write(number3);
+document.write(number4);
+document.write(number5);
+document.write(number6);
+document.write(number7);
+document.write(result1);
+document.write(result2);*/
+//document.write(`${home} ${domen} ${domen1} ${domen2} ${number} ${number2} ${number2} ${number3} ${number4} ${number5} ${number5} ${number6} ${number7} ${result1} ${result2}`);
+
+
+//Завдання №2
+
+
+/*
+let userName = 'andriy';
+let userBrotherName = 'mikola';
+let helloWorld = 'hello';
+let userSurName = 'zub';
+let userAge = 25;
+let numberHouse = 2;
+let numberHouse1 = 144;
+let numberHouse2 = -833;
+let numberHouse3 = 833;
+let numberHouse4 = 8.3;
+let numberHouse5 = 2.8;
+let value = true;
+let value1 = false;
+
+
+console.log(userName);
+console.log(userBrotherName);
+console.log(helloWorld);
+console.log(userSurName);
+console.log(userAge);
+console.log(numberHouse);
+console.log(numberHouse1);
+console.log(numberHouse2);
+console.log(numberHouse3);
+console.log(numberHouse4);
+console.log(numberHouse5);
+console.log(value);
+console.log(value1);
+*/
+
+/*alert(userName);
+alert(userBrotherName);
+alert(helloWorld);
+alert(userSurName);
+alert(userAge);
+alert(numberHouse);
+alert(numberHouse1);
+alert(numberHouse2);
+alert(numberHouse3);
+alert(numberHouse4);
+alert(numberHouse5);
+alert(value);
+alert(value1);*/
+
+
+/*document.write(userName);
+document.write(userBrotherName);
+document.write(helloWorld);
+document.write(userSurName);
+document.write(userAge);
+document.write(numberHouse);
+document.write(numberHouse1);
+document.write(numberHouse2);
+document.write(numberHouse3);
+document.write(numberHouse4);
+document.write(numberHouse5);
+document.write(value);
+document.write(value1);*/
+//document.write(`${userName} ${userBrotherName} ${helloWorld} ${userSurName} ${userAge} ${numberHouse} ${numberHouse1} ${numberHouse2} ${numberHouse3} ${numberHouse4} ${numberHouse5} ${value} ${value1}`)
+
+
+                     //Завдання №3
+/*const value = 'document';
+const value1 = 'document1';
+const value2 = 'document2';
+const userAge = 12;
+const userAgeHouse = -8
+const userAgeCar = 23
+
+
+console.log(value);
+console.log(value1);
+console.log(value2);
+console.log(userAge);
+console.log(userAgeHouse);
+console.log(userAgeCar);*/
+
+/*alert(value);
+alert(value1);
+alert(value2);
+alert(userAge);
+alert(userAgeHouse);
+alert(userAgeCar);
+
+
+document.write(value);
+document.write(value1);
+document.write(value2);
+document.write(userAge);
+document.write(userAgeHouse);
+document.write(userAgeCar)*/
+
+/*document.write(`${value} ${value1} ${value2} ${userAge} ${userAgeHouse} ${userAgeCar}`);*/
+
+                          //Завдання №4
+
+/*let name = 'andriy';
+let surName = 'mayboroda';
+let fatherName = 'oleksandrovich';
+
+/!*prompt(name);
+prompt(surName);
+prompt(fatherName);*!/
+
+console.log(name);
+console.log(surName);
+console.log(fatherName)
+
+alert(name);
+alert(surName);
+alert(fatherName);
+
+/!*document.write(name);
+document.write(surName);
+document.write(fatherName);*!/
+
+document.write(`${name} ${surName} ${fatherName}`);*/
+
+
+                                         //Завдання №5
+
+/*let name = 'andriy';
+let surName = 'mayboroda';
+let fatherName = 'oleksandrovich';
+
+let person = name + surName + fatherName ;
+console.log(person);*/
+
+
+                                     //Завдання №6
+
+
+/*let fatherName = 'oleksandr';
+let matherName = 'olya';
+let brotherName = 'kolya';
+
+prompt(fatherName);
+prompt(matherName);
+prompt(brotherName);
+
+alert(fatherName);
+alert(matherName);
+alert(brotherName);
+
+document.write(fatherName);
+document.write(matherName);
+document.write(brotherName);
+
+document.write(`${fatherName} ${matherName} ${brotherName}`);
+
+let person = fatherName + matherName + brotherName;
+console.log(fatherName);
+console.log(matherName);
+console.log(brotherName);*/
+
+
+
+                                //Завдання №7
+
+
+
+/*let fatherAge = +prompt('number55');
+let matherAge = +prompt('number42');
+let brotherAge = +prompt('number22');
+
+console.log(fatherAge);
+console.log(matherAge);
+console.log(brotherAge);*/
+
+
+                                    //Завдання №8
+// let fatherAge = parseInt(prompt('age55'));
+// let matherAge = parseInt(prompt('age42'));
+// let brotherAge = parseInt(prompt('age22'));
+// let mayAge = parseInt(prompt('age26'));
+//
+//
+// let family = fatherAge + matherAge + brotherAge + mayAge;
+// console.log(family);
+//
+// let result = 145;
+// console.log(result);
+// console.log(fatherAge);
+// console.log(matherAge);
+// console.log(brotherAge);
+//console.log(mayAge);
+
+                                //Завдання №9
+
+/*
+let father = parseFloat(prompt('shoeNumber23.5'));
+let mather = parseFloat(prompt('shoeNumber24.3'));
+let brother = parseFloat(prompt('shoeNumber23.5'));
+
+
+console.log(father);
+console.log(mather);
+console.log(brother);
+
+
+let family = father + mather + brother;
+console.log(family);
+let result = 71.3;
+console.log(result);
+*/
+
+
+
+             //Завдання №10
+
+
+/*
+let father = Math.round(prompt('shoeNumber23.5'));
+let mather = Math.round(prompt('shoeNumber24.3'));
+let brother = Math.round(prompt('shoeNumber23.5'));
+
+console.log(father);
+console.log(mather);
+console.log(brother);
+
+
+let family = father + mather + brother;
+console.log(family);
+let result = 72;
+console.log(result);
+*/
+
+
+               //Завдання №11
+
+
+/*let father = prompt('shoeNumber2');
+let mather = prompt('shoeNumber3');
+let family = Math.pow(2, 3);
+console.log(family);
+
+let result = 8;
+console.log(result);*/
+
+
+                      //Завдання № 12
+
+/*
+
+let a = 100; number
+let b = '100';string
+let c = true; boolean
+let d = undefined;undefined
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+*/
+
+                        //Завдання №13
+
+/*5 < 6 -> true
+5 > 6 -> false
+5 = 6 -> false
+5 === 6 -> false
+10 = 10 -> true
+10 === 10 -> true
+10 < 10 -> false
+10 > 10 -> false
+10 != 10 -> false
+123 === '123' -> false
+123 == '123' -> true*/
+
+
+                     //Завдання №14
+
+/*
+console.log(132 > 100 && 45 < 12 ); False
+console.log(34 > 33 && 23 < 90 );True
+console.log(99 > 100 && 45 > 12 );False
+console.log(132 > 100 || 45 < 12 );True
+console.log(111 > 11 || 45 < 111 );True
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );False
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );True
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );False
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));True
+console.log(!!'-1');True
+console.log(!!-1);True
+console.log(!!'0');True
+console.log(!!'null');True
+console.log(!!'undefined');True
+console.log(!!(3/'owu'));False
+console.log((111 > 11 || 45 < 111) ||  !!'0');TRue
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));False
+
+*/
+
+
+
+// CLASS
+
+
+                          //Задача 1
+
+
+/*let str = 'Привет'
+let num = 123
+let flag = true
+let txt = 'true'
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)*/
+
+
+                       //Задача 2
+
+/*
+let a1 = 5 + 3
+let a2 = 5 - 3
+let a3 = 5 * 3
+let a4 = 5 / 3
+let a5 = 5 % 3
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+*/
+
+                  //Задача 3
+/*
+
+let a6 = 5 % 3
+let a7 = 3 % 5
+let a8 = 5 + '3'
+let a9 = '3' + 5
+let a10 = '5' - 3
+let a11 = 75 + 'кг'
+
+console.log(a6)
+console.log(a7)
+console.log(a8)
+console.log(a9)
+console.log(a10)
+console.log(a11)
+*/
+
+                      //Задача 4
+
+/*s=ab*/
+
+/*let width = 10;
+let height = 23;
+let s = (width * height);
+
+console.log(s)*/
+
+                       //Завдання №5
+
+/*let d = 4;
+let height = 10;
+
+let r = d / 2;
+console.log(r);
+let result = 2;
+let p = 3.14;
+
+let v = p * Math.pow(2,2) * height;
+console.log(v)
+result = 125.6*/
+
+
+                       //Завдання №6
+
+
+/*
+let n = 3
+let m = 4
+let k = Math.sqrt( Math.pow(3 , 2) + Math.pow(4 , 2));
+console.log(k)*/
+
+
+                       //Завдання №7
+
+
+/*
+
+let str = 'Hello World';
+
+console.log(str)
+document.write(str)
+alert(str)
+
+*/
+
+
+                         //Завдання №8
+
+/*alert('Майборода Андрій Олекандрович \ n25 \ football');*/
+
+                           //Завдання №9
+
+/*
+let str1 = 'Кто';
+let str2 = 'ти';
+let str3 = 'такой?';
+let concatenation = str1 + str2 +str3;
+console.log(concatenation)
+document.write(`${str1} ${str2} ${str3}`)*/
+
+                            //Завдання №10
+
+
+//Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?почемy
+/*    let str = "20";
+let a = 5;
+document.write(str + a + "<br/>"); 205
+document.write(str - a + "<br/>"); 15
+document.write(str * "2" + "<br/>");40
+document.write(str / 2 + "<br/>");10
+  ВСі значення виведе / тому що він все додає все віднімає робить все строчним*/
+
+                                    //Завдання №11
+/*Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?*/
+/*
+  let a = parseInt("3.14");
+console.log(a);                      3
+  let b = parseInt("-7.875");
+console.log(b);                   -7
+let c = parseInt("435");
+console.log(c);                  435
+let d =  parseInt("Вася");
+console.log(d);                     nan
+
+
+*/
+
+                                                        //Завдання №12
+
+//С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные, которые будут использоваться далее, повторите код ниже
+
+/*let str =prompt("Enter something", "ho-ho" );
+console.log(str);*/
+
+                                                            //Завдання №13
+
+
+/*
+С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert
+*/
+
+/*let str = prompt('10 + 20');
+alert(str)*/
+                                                //Завдання №14
+/*
+
+С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+
+
+let str1 = prompt('What is your name?' ,'Иван' );
+let str2 = prompt('What is your surName?' , 'Иванов');
+let str3 = prompt('How old are you?', '28');
+alert(' Доброго вечера Иван Иванов, мои поздравления что вам 28');
+
+
+*/
+
+                                    //Завдання №15
+
+/*
+let number1 =+prompt('value8')
+let number2 = +prompt('value4')
+let number3 = +prompt('value2')
+
+if (number1 > number2 && number1 > number3) {
+    if (number2 > number3) {
+        console.log(number3, number2, number1)
+    }
+    else {console.log(number2, number3,number1)}
+}
+if (number1 < number2 && number2 > number3) {
+    if (number1 < number3) {
+        console.log(number1, number3, number2)
+    }
+    else {console.log(number3, number1, number2)}
+}
+if (number3>number2 && number1<number3) {
+    if (number2 > number1) {
+        console.log(number1, number2, number3)
+    }
+    else console.log(number2, number1, number3)
+}
+
+*/
+
+
+
+                                                        //Завдання №16
+
+
+/*
+let trafficLight = prompt("Світлофор работает", "Світловор в аварійному стані");
+if (trafficLight === 'Зелений' ) {
+    alert('Йди')
+}else if (trafficLight === 'Жовтий') {
+    alert('Чекай')
+}else if (trafficLight === 'Червони') {
+    alert('Стій')
+}
+
+*/
+
+                                                                //Завдання №17
+let  trafficLight = prompt("Які кольора світловора", 'в аварийном режиме');
+let isRoadClear = confirm('Есть машина?')
+/*if (trafficLight === 'зелений' && isRoadClear === false) {
+    alert('йди')
+}else if (trafficLight === 'зелений' && isRoadClear === true ) {
+    alert('подожди пока нарушители проедут')
+}*/
+/*if (trafficLight === 'жовтий' && isRoadClear === true) {
+    alert('чекай')
+} else if ( trafficLight === 'жовтий' && isRoadClear === false) {
+    alert('все рано жди')
+}*/
+if (trafficLight === 'червоний' && isRoadClear === false) {
+    alert('чекай')
+} else if (trafficLight === 'червоний' && isRoadClear === true) {
+    alert('стой і чекай')
+}else if (trafficLight === 'в аварийном режиме') {
+    alert('роби що хочеш')
+}
+
+
+
+
+
+
+
+
+
+
